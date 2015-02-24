b14b81d76fe06a1651eca4bf7cba2dfc84f98380
fupan.li <fupan.li@windriver.com>
2015-02-24 21:32:05 +0800

    add macro

--- FiveInRowServer.py
+++ FiveInRowServer.py
@@ -17,6 +17,7 @@ DRAW = 0
 CONTINUE = 1
 WIN = 2
 ERROR = 3
+TIMEOUT = 100
 
 class Game(threading.Thread):
     
@@ -43,7 +44,7 @@ class Game(threading.Thread):
         self.pos.extend([['_']*CHESS_BOARD_BLOCK_COUNTS for foo in range(CHESS_BOARD_BLOCK_COUNTS)])
 
 	current_role = 0
-        timeout = 50
+        timeout = TIMEOUT
 	while game_not_over:
         # get data
             print "--------------running "+str(self.data_name) + "--------timeout="+str(timeout)
@@ -57,7 +58,7 @@ class Game(threading.Thread):
                     game_not_over = False
 	        continue
 	    else:
-                timeout = 50
+                timeout = TIMEOUT
 	        current_role = data['SeqID'] % 2
 	        self.current_pawn = current_role^1
 	        X = data['PosX']
